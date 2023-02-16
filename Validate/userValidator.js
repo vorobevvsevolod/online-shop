6f379d3e2fe6b1bb8b6f0e7375adfb0bc2f952f5
Vsevolod <seva31122003@mail.ru>
2023-02-16 21:07:36 +0300

    backend 1.1

--- Validate/userValidator.js
+++ Validate/userValidator.js
@@ -4,6 +4,7 @@ const registrationValidator = [
     body('password', "Пароль должен быть минимум 5 символов").isLength({min: 5}),
     body('username', "Имя должно быть минимум 3 символа").isLength({min: 3})
 ]
+
 const LoginValidator = [
     body('email', "Неверный формат почты").isEmail(),
     body('password', "Пароль должен быть минимум 5 символов").isLength({min: 5})
